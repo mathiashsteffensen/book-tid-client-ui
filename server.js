fba44d727fde162024ee22e9236a9be7d1aaf4f3
Mathias <mathias@mathiashsteffensen@protonmail.com>
2020-12-14 20:23:01 +0100

    fixed production script to a custom server that enforces https

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-import next from 'next';
-import sslRedirect from 'heroku-ssl-redirect';
-import express from 'express';
+const next = require('next');
+const express = require('express');
+const http = require('http');
+const express_enforces_ssl = require('express-enforces-ssl');
 
-const port = parseInt(process.env.PORT, 10) || 3000;
+const port = process.env.PORT || 3000;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
@@ -11,14 +12,15 @@ app.prepare().then(() => {
   const server = express();
 
   // redirect to SSL
-  server.use(sslRedirect());
+  server.enable('trust proxy');
+ 
+  server.use(express_enforces_ssl());
 
   server.all('*', (req, res) => {
     return handle(req, res);
   });
 
-  server.listen(port, err => {
-    if (err) throw err;
-    console.log(`> Ready on http://localhost:${port}`);
-  });
+  http.createServer(app).listen(port, function() {
+    console.log('Express server listening on port ' + port);
+  },);
 });
