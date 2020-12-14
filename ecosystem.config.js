0fc2dcef3b219438b0e175afc0e77b46fb00481c
Mathias <mathias@mathiashsteffensen@protonmail.com>
2020-12-14 19:56:28 +0100

    fixed production script to a custom server that enforces https

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
     name: "BOOK-TID-API",
-    script: "./server.js",
+    script: "./server.mjs",
     instances: "max",
     env: {
       NODE_ENV: "development",
